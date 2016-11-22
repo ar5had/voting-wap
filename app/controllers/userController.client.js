d74ce07a7a635d4cbdd842d124986e90cee104e2
Arshad Khan <arshdkhn1@gmail.com>
2016-11-22 16:42:33 +0000

    editing

--- app/controllers/userController.client.js
+++ app/controllers/userController.client.js
@@ -5,9 +5,9 @@
    var profileId = document.querySelector('#profile-id') || null;
    var profileUsername = document.querySelector('#profile-username') || null;
    var profileRepos = document.querySelector('#profile-repos') || null;
-   var displayName = document.querySelector('#display-name');
+   var name = document.querySelector('#display-name');
    var apiUrl = appUrl + '/api/:id';
-   console.log(profileId, profileRepos, profileUsername);
+   
    function updateHtmlElement (data, element, userProperty) {
       element.innerHTML = data[userProperty];
    }
@@ -15,10 +15,10 @@
    ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function (data) {
       var userObject = JSON.parse(data);
       console.log(userObject);
-      if (userObject.displayName !== null) {
-         updateHtmlElement(userObject, displayName, 'displayName');
+      if (userObject.name !== null) {
+         updateHtmlElement(userObject, name, 'name');
       } else {
-         updateHtmlElement(userObject, displayName, 'username');
+         updateHtmlElement(userObject, name, 'username');
       }
 
       if (profileId !== null) {
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
 app.use(session({
-	secret: 'secretClementine',
+	secret: process.env.SESSION_SECRET,
 	resave: false,
 	saveUninitialized: true
 }));
