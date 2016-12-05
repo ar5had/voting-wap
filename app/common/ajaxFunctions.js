3b3c33aba3b6d351530bff9d11cf70a551554700
Arshad Khan <arshdkhn1@gmail.com>
2016-12-05 00:35:45 +0000

    removing bug

--- app/common/ajaxFunctions.js
+++ app/common/ajaxFunctions.js
@@ -31,7 +31,8 @@ var ajaxFunctions = {
       xmlhttp.onreadystatechange = function () {
          if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
             callback(true);
-         } else {
+         } else if (xmlhttp.readyState === 4 && xmlhttp.status > 200) {
+            console.log("rs called");
             callback(false);
          }
          
